b660ba323ebea277d41bc158c89a3efe6afee8b7
Momen Mohamed <mo2men2000@gmail.com>
2023-12-28 01:00:56 +0200

    Add Fileds in Rooms

--- lib/features/rooms/widget/room_view_body.dart
+++ lib/features/rooms/widget/room_view_body.dart
@@ -21,7 +21,11 @@ class RoomViewBody extends StatefulWidget{
 class _RoomViewBody extends State<RoomViewBody> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+  String framePhoto="";
+  final isSeatClosedNotifier = ValueNotifier<bool>(false);
+  final isRequestingNotifier = ValueNotifier<bool>(false);
+  final controller = ZegoLiveAudioRoomController();
+  List<IconData> customIcons = [Icons.cookie, Icons.phone, Icons.speaker, Icons.air, Icons.blender, Icons.file_copy, Icons.place, Icons.phone_android, Icons.phone_iphone];
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -46,7 +50,7 @@ class _RoomViewBody extends State<RoomViewBody> {
         return StreamBuilder<QuerySnapshot>(
             stream: _firestore.collection('store').where('id',isEqualTo: Myframe).snapshots(),
             builder: (context,snapshot){
-              String framePhoto="";
+              
               if (!snapshot.hasData) {
                 return const Center(
                   child: CircularProgressIndicator(
@@ -107,11 +111,217 @@ class _RoomViewBody extends State<RoomViewBody> {
                           ZegoLiveAudioRoomLayoutRowConfig(count: 4, alignment: ZegoLiveAudioRoomLayoutAlignment.spaceAround),
                           ZegoLiveAudioRoomLayoutRowConfig(count: 4, alignment: ZegoLiveAudioRoomLayoutAlignment.spaceAround),
                         ]
+                        ..bottomMenuBarConfig.audienceButtons = const [
+                          ZegoMenuBarButtonName.showMemberListButton,
+                        ]
+                        // ..seatConfig = (ZegoLiveAudioRoomSeatConfig()
+                        //   ..backgroundBuilder = backgroundBuilder
+                        //   ..foregroundBuilder = foregroundBuilder),
+                        ..onSeatClicked = (int index, ZegoUIKitUser? user) {
+                          showDemoBottomSheet(context);
+                        }
+                        ..bottomMenuBarConfig.audienceExtendButtons = [
+                          connectButton(),
+                        ]
+                        ..onSeatClosed = () {
+                          isSeatClosedNotifier.value = true;
+                        }
+                        ..onSeatsOpened = () {
+                          isSeatClosedNotifier.value = false;
+                        }
+                        ..onSeatsChanged = (
+                            Map<int, ZegoUIKitUser> takenSeats,
+                            List<int> untakenSeats,
+                            ) {
+                          if (isRequestingNotifier.value) {
+                            if (takenSeats.values
+                                .map((e) => e.id)
+                                .toList()
+                                .contains(widget.userid)) {
+                              /// on the seat now
+                              isRequestingNotifier.value = false;
+                            }
+                          }
+                        }
+                        ..innerText.memberListTitle = 'Members'
+                        ..onSeatTakingRequestFailed = () {
+                          isRequestingNotifier.value = false;
+                        }
+                        ..onSeatTakingRequestRejected = () {
+                          isRequestingNotifier.value = false;
+                        }
+                        ..bottomMenuBarConfig = ZegoBottomMenuBarConfig(
+                          maxCount: 5,
+                          hostButtons: [
+                            ZegoMenuBarButtonName.toggleMicrophoneButton,
+                            ZegoMenuBarButtonName.showMemberListButton,
+                          ],
+                          hostExtendButtons: [
+                            for (int i = 0; i < customIcons.length; i++)
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(40, 40),
+                                  shape: const CircleBorder(),
+                                  primary: const Color(0xff2C2F3E).withOpacity(0.6),
+                                ),
+                                onPressed: () {},
+                                child: Icon(customIcons[i]),
+                              ),
+                          ],
+                          speakerButtons: [
+                            ZegoMenuBarButtonName.toggleMicrophoneButton,
+                            ZegoMenuBarButtonName.showMemberListButton,
+                          ],
+                        )
+                        ..onMemberListMoreButtonPressed = (ZegoUIKitUser user) {
+                          showDemoBottomSheet(context);
+                        },
+                      controller: controller,
+
                   ));
             }
         );
       },
     );
+  }
+  Widget connectButton() {
+    return ValueListenableBuilder<bool>(
+      valueListenable: isSeatClosedNotifier,
+      builder: (context, isSeatClosed, _) {
+        return isSeatClosed
+            ? ValueListenableBuilder<bool>(
+          valueListenable: isRequestingNotifier,
+          builder: (context, isRequesting, _) {
+            return isRequesting
+                ? ElevatedButton(
+              onPressed: () {
+                controller.cancelSeatTakingRequest().then((result) {
+                  isRequestingNotifier.value = false;
+                });
+              },
+              child: const Text('Cancel'),
+            )
+                : ElevatedButton(
+              onPressed: () {
+                controller.applyToTakeSeat().then((result) {
+                  isRequestingNotifier.value = result;
+                });
+              },
+              child: const Text('Request'),
+            );
+          },
+        )
+            : Container();
+      },
+    );
+  }
+  void showDemoBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      backgroundColor: const Color(0xff111014),
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(32.0),
+          topRight: Radius.circular(32.0),
+        ),
+      ),
+      isDismissible: true,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return AnimatedPadding(
+          padding: MediaQuery.of(context).viewInsets,
+          duration: const Duration(milliseconds: 50),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  height: 40,
+                  child: Center(
+                    child: Text(
+                      'Menu $index',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+  bool isAttributeHost(Map<String, String>? userInRoomAttributes) {
+    return (userInRoomAttributes?[attributeKeyRole] ?? "") ==
+        ZegoLiveAudioRoomRole.host.index.toString();
+  }
+  Widget backgroundBuilder(
+      BuildContext context, Size size, ZegoUIKitUser? user, Map extraInfo) {
+    if (!isAttributeHost(user!.inRoomAttributes as Map<String, String>?)) {
+      return Container();
+    }
+
+    return Positioned(
+      top: -8,
+      left: 0,
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image:DecorationImage(
+              image: CachedNetworkImageProvider(framePhoto)
+          )
+        ),
+      ),
+    );
+  }
+  Widget foregroundBuilder(
+      BuildContext context, Size size, ZegoUIKitUser? user, Map extraInfo) {
+    var userName = user?.name.isEmpty ?? true
+        ? Container()
+        : Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Text(
+        user?.name ?? "",
+        overflow: TextOverflow.ellipsis,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          backgroundColor: Colors.black.withOpacity(0.1),
+          fontSize: 9,
+          fontWeight: FontWeight.w600,
+          decoration: TextDecoration.none,
+        ),
+      ),
+    );
+
+    if (!isAttributeHost(user!.inRoomAttributes as Map<String, String>?)) {
+      return userName;
+    }
+
+    var hostIconSize = Size(size.width / 3, size.height / 3);
+    var hostIcon = Positioned(
+      bottom: 3,
+      right: 0,
+      child: Container(
+        width: hostIconSize.width,
+        height: hostIconSize.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: CachedNetworkImageProvider(_auth.currentUser!.photoURL.toString())
+          ),
+        ),
+      ),
+    );
+
+    return Stack(children: [userName, hostIcon]);
   }
 
   Widget background() {
